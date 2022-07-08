b9f00cda209dfcd44d66be3a3b6ccf0400b86035
SilentBudd <95491928+SilentBudd@users.noreply.github.com>
2022-07-08 20:45:22 +0100

    user schema WIP

---
+++ schemas/user.js
@@ -0,0 +1,49 @@
+export default {
+    name: "user",
+    title: "User",
+    type: "document",
+    fields: [
+        {
+            name: "name",
+            title: "Name",
+            type: "string"
+        },
+        {
+            name: "username",
+            title: "Username",
+            type: "string"
+        },
+        {
+            name: "email",
+            title: "Email",
+            type: "string"
+        },
+        {
+            name: "password",
+            title: "Password",
+            type: "password"
+        },
+        {
+            name: "photo",
+            title: "Photo",
+            type: "image"
+        },
+        {
+            name: "following",
+            title: "Following",
+            type: "array",
+            of: [
+                {
+                    type: "reference",
+                    to: [{ type:"user" }]
+                }
+            ],
+            validation: (Rule) => Rule.unique(),
+        },
+        {
+            name: "created_at",
+            title: "Created At",
+            type: "datetime",
+        },
+    ],
+};
